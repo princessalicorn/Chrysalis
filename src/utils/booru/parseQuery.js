c89f764aa2e5271becc4ed127c8fef7b8b5a98b6
programmerpony <programmerpony@riseup.net>
2022-01-25 15:53:06 +0100

    Ultimate parser

--- src/utils/booru/parseQuery.js
+++ src/utils/booru/parseQuery.js
@@ -2,6 +2,7 @@ module.exports = async (message, args) => {
   let query = '';
   if (args.length>0) query = message.author ? args.join(' ') : args[0];
   while (query.includes(' ,')) query = query.replace(' ,',',');
+  while (query.includes(', ')) query = query.replace(', ',',');
   while (query.includes(',,')) query = query.replace(',,',',');
   if (query.startsWith(',')) query = query.substring(1,query.length);
   if (query.endsWith(',')) query = query.substring(0,query.length-1);
